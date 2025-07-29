5767028b867bf90ce6395d9c18fbe11697313c79
Mauricio Cox <coxmau77@gmail.com>
2025-07-29 08:33:47 -0300

    chore: Ajustes en el grid de lista de productos

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import styles from './ProductList.module.css';
+import Button from '../Button';
 
 function ProductList() {
     const [products, setProducts] = useState([]);
@@ -16,24 +17,37 @@ function ProductList() {
     }, []);
 
     return (
-        <div className={styles.productList}>
-            {loading ? (
-                <p>Cargando...</p>
-            ) : (
-                <ul>
-                    {products.map(product => (
-                        <li key={product.id}>
-                            <img src={product.image} alt={product.title} />
-                            <h2>{product.title}</h2>
-                            <span>{product.category}</span> <br />
-                            <span>{product.rating.rate} - ({product.rating.count})</span>
-                            <p>{product.description}</p>
-                            <p>Precio: ${product.price}</p>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
+        <>
+            <h1>Productos</h1>
+            <p>Esta es una lista de productos obtenida de la Fake Store API. Puedes ver detalles de cada producto, incluyendo su imagen, título, categoría, calificación, descripción y precio. También puedes comprar los productos haciendo clic en el botón "Comprar ahora".</p>
+            <div className={styles.productList}>
+                {loading ? (
+                    <p>Cargando...</p>
+                ) : (
+                    <ul>
+                        {products.map(product => (
+                            <li key={product.id}>
+                                <img src={product.image} alt={product.title} />
+                                <div>
+                                    <h2>
+                                        {product.title.split(' ').slice(0, 6).join(' ')}
+                                        {product.title.split(' ').length > 6 ? '...' : ''}
+                                    </h2>
+                                    <span>{product.category}</span> <br />
+                                    <span>{product.rating.rate} - ({product.rating.count})</span>
+                                    <p>
+                                        {product.description.split(' ').slice(0, 12).join(' ')}
+                                        {product.description.split(' ').length > 12 ? '...' : ''}
+                                    </p>
+                                    <p>Precio: ${product.price}</p>
+                                    <Button buttonText="Comprar ahora" />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </>
     );
 }
 
